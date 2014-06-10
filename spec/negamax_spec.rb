0c037862d930888116ae9970d488b9b2d339ef00
Dom Leone <ohheydom@gmail.com>
2014-06-10 19:53:07 -0400

    Negamax appears to be working, although at similar speeds to minimax.

--- spec/negamax_spec.rb
+++ spec/negamax_spec.rb
@@ -3,10 +3,68 @@ require_relative '../tic_tac_toe/check_winner'
 
 describe Negamax do
   describe '#negamax' do
+    it 'returns 100 if x wins' do
+      board = %w(x x x - - - - - -)
+      game_board = double('GameBoard', board: board)
+      check_winner = CheckWinner
+      negamax = Negamax.new(game_board, 'o', check_winner)
+      expect(negamax.negamax(board)).to eq(100)
+    end
 
+    it 'returns -100 if o wins' do
+      board = %w(o o o - - - - - -)
+      game_board = double('GameBoard', board: board)
+      check_winner = CheckWinner
+      negamax = Negamax.new(game_board, 'o', check_winner)
+      expect(negamax.negamax(board)).to eq(-100)
+    end
+
+    it 'returns 0 if nobody wins' do
+      board = %w(x o x o x o o x o)
+      game_board = double('GameBoard', board: board)
+      check_winner = CheckWinner
+      negamax = Negamax.new(game_board, 'o', check_winner)
+      expect(negamax.negamax(board)).to eq(0)
+    end
+
+    it 'returns 99 if x can win' do
+      board = %w(x - x - o - o - -)
+      game_board = double('GameBoard', board: board)
+      check_winner = CheckWinner
+      negamax = Negamax.new(game_board, 'o', check_winner)
+      expect(negamax.negamax(board)).to eq(99)
+    end
+
+    it 'returns -99 if o can win' do
+      board = %w(o - o - x - x - -)
+      game_board = double('GameBoard', board: board)
+      check_winner = CheckWinner
+      negamax = Negamax.new(game_board, 'o', check_winner)
+      negamax.switch_turn
+      expect(negamax.negamax(board)).to eq(-99)
+    end
   end
 
   describe '#best_move' do
+    it 'returns the location for a winning move for x' do
+      board = %w(x - -
+                 x - o
+                 - o -)
+      game_board = double('GameBoard', board: board)
+      check_winner = CheckWinner
+      negamax = Negamax.new(game_board, 'o', check_winner)
+      expect(negamax.best_move).to eq(6)
+    end
 
+    it 'returns the location for a winning move for o' do
+      board = %w(- - x
+                 o - o
+                 - x -)
+      game_board = double('GameBoard', board: board)
+      check_winner = CheckWinner
+      negamax = Negamax.new(game_board, 'o', check_winner)
+      negamax.switch_turn
+      expect(negamax.best_move).to eq(4)
+    end
   end
 end
--- tic_tac_toe/algorithms/negamax.rb
+++ tic_tac_toe/algorithms/negamax.rb
@@ -1,3 +1,83 @@
 class Negamax
+  attr_reader :check_winner, :computer_player
+  attr_accessor :current_turn, :board
+  O = 'o'
+  X = 'x'
 
+  def initialize(game_board, computer_player, check_winner)
+    @board = game_board.board
+    @dup = board.dup
+    @check_winner = check_winner
+    @current_turn = computer_player
+    @computer_player = computer_player
+  end
+
+  def negamax(board, depth = 0, color = 1)
+    switch_turn
+    if game_over? 
+      return score(depth) * color
+    end
+    best_score = -Float::INFINITY
+    remaining_indices.each do |move|
+      new_board = new_move(move)
+      score = -negamax(new_board, depth + 1, xturn(-color, color))
+      undo_move(move)
+      switch_turn
+      best_score = [best_score, score].max
+    end
+    xturn(best_score, -best_score)
+  end
+
+  def best_move
+    negamax_moves.send(xturn(:max_by, :min_by)) { |score, move| score }[1]
+  end
+
+  def switch_turn
+    @current_turn = current_turn == X ? O : X
+  end
+
+  def undo_move(location)
+    @dup[location] = '-'
+  end
+
+  def remaining_indices
+    @dup.each_index.select { |ind| @dup[ind] == '-' }
+  end
+
+  private
+
+  def game_over?
+    check_winner.new(@dup, human_player).win? ||
+    check_winner.new(@dup, computer_player).win? ||
+    remaining_indices.empty?
+  end
+
+  def human_player
+    computer_player == O ? X : O
+  end
+
+  def negamax_moves
+    remaining_indices.map do |move|
+      new_move(move)
+      score = [negamax(board), move]
+      undo_move(move)
+      switch_turn
+      score
+    end
+  end
+
+  def new_move(location)
+    @dup[location] = current_turn
+    @dup
+  end
+
+  def score(depth)
+    return (100 - depth) if check_winner.new(@dup, human_player).win?
+    return (depth - 100) if check_winner.new(@dup, computer_player).win?
+    return 0 if remaining_indices.empty?
+  end
+
+  def xturn(value_x, value_o)
+    current_turn == human_player ? value_x : value_o
+  end
 end
